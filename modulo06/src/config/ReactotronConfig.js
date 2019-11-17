394a00c170c783847fcf0b5a179f96be6c50d771
Douglas-Marques <douglasmarques09@gmail.com>
2019-11-17 13:02:49 -0300

    novos projetos

---
+++ modulo06/src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-native';
+import {NativeModules} from 'react-native';
+
+// grabs the ip address
+const host = NativeModules.SourceCode.scriptURL.split('://')[1].split(':')[0];
+
+const reactotron = Reactotron.configure({host})
+  .useReactNative()
+  .connect();
+
+export default reactotron;
---
+++ modulo06/src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import {Container} from './Styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <Container />;
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
